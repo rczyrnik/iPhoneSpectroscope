7c48e991c895b07bfb06fad8420c0b8278595c18
rczyrnik <rczyrnik@gmail.com>
2017-01-14 21:13:35 -0500

    Reformatted program, automated elements and saving

--- Smoothing.py
+++ Smoothing.py
@@ -4,15 +4,23 @@ multiple times and graphing the result
 
 """
 import matplotlib.pyplot as plt
-
+import sys
 
 from iPhoneSpectroscope import iPhoneSpectroscope
-x = []
-y = []
-for i in range(1, 10):
-    peakLambda, peakIntensity = iPhoneSpectroscope(3, i)
-    x.append(i)
-    y.append(len(peakLambda))
 
-plt.plot(x, y)
-plt.show()
+def Smoothing(element):
+    x = []
+    y = []
+    for i in range(1, 100):
+        peakLambda, peakIntensity = iPhoneSpectroscope(element, i)
+        x.append(i)
+        y.append(len(peakLambda))
+    plt.plot(x, y)
+    plt.title("Element "+str(element))
+    plt.savefig("Element"+str(element)+".png", bbox_inches='tight')
+    plt.clf()
+
+if __name__ == '__main__':
+#    Element = sys.argv[1]
+    for e in range(1, 9):
+        Smoothing(e)
